96ff47b2e7d384cf5c81607499b11aef2cb6c8ee
Prothom <prothomghosh41@gmail.com>
2026-03-27 14:34:06 +0600

    Initial Commit

---
+++ Stella/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Stella
+//
+//  Created by prothom on 3/18/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Stella/StellaApp.swift
@@ -0,0 +1,32 @@
+//
+//  StellaApp.swift
+//  Stella
+//
+//  Created by prothom on 3/18/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StellaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
